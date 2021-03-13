931c76f1211d506d53b4d7ec9210b084609d6a3e
PaulVR4 <63301418+PaulVR4@users.noreply.github.com>
2021-03-13 10:34:49 +0200

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id:1,
+    name: 'Adam Surub',
+    username: 'Tinichia',
+    email: 'adam@fiervechi.crs'
+  },
+  {
+    id:2,
+    name: 'Barbu Surub',
+    username: 'Ruginitul',
+    email: 'barbu@fiervechi.crs'
+  },
+  {
+    id:3,
+    name: 'Costel Surub',
+    username: 'Tetanos',
+    email: 'costel@fiervechi.crs'
+  },
+  {
+    id:4,
+    name: 'David Surub',
+    username: 'Rusty',
+    email: 'david@fiervechi.crs'
+  },
+  {
+    id:5,
+    name: 'Eusebiu Surub',
+    username: 'Tinichia47',
+    email: 'eusebiu@fiervechi.crs'
+  },
+  {
+    id:6,
+    name: 'Fodor Surub',
+    username: 'Nemtudom',
+    email: 'fodor@fiervechi.crs'
+  },
+  {
+    id:7,
+    name: 'Gogaie Surub',
+    username: 'Grason',
+    email: 'gogaie@fiervechi.crs'
+  },
+  {
+    id:8,
+    name: 'Harold Piulita',
+    username: 'Gaurinceanu',
+    email: 'harold@fiervechi.crs'
+  },
+  {
+    id:9,
+    name: 'Ion Surub',
+    username: 'Iobag33',
+    email: 'ion@fiervechi.crs'
+  },
+  {
+    id:10,
+    name: 'Jean Cui',
+    username: 'Intepatul',
+    email: 'jean@fiervechi.crs'
+  },
+];
